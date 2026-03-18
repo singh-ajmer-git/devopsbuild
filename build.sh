50097189f7f0be2565eb7eb7965ee805ba8631d5
root <root@ip-172-31-30-99.us-west-2.compute.internal>
2026-03-18 19:54:58 +0000

    test 11

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-docker build -t react-app .
+
+# Build Docker image
+docker build -t $DOCKERHUB_USER/react-app:latest .
+
+# Optional: tag image again
+docker tag $DOCKERHUB_USER/react-app:latest $DOCKERHUB_USER/react-app:latest
+
+# Login to Docker Hub using credentials from Jenkins
+echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin
+
+# Push image
+docker push $DOCKERHUB_USER/react-app:latest
